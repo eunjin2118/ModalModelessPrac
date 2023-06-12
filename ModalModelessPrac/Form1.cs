2e7ef9acf06f21ae35e26c1f85f0043dd3f311a1
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-06-12 12:02:38 +0900

    메시비박스를 활용한 모달창 띄우기 및 버튼입력 받기

--- ModalModelessPrac/Form1.cs
+++ ModalModelessPrac/Form1.cs
@@ -16,5 +16,21 @@ namespace ModalModelessPrac
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel); 
+            } while (result == DialogResult.Retry);
+            if(result == DialogResult.Cancel)
+            {
+                MessageBox.Show("사용자가 취소하였습니다", "알림");
+            }
+        }
     }
 }
